0b36a3399f21955afe06e8b86a99b696e305b0b0
Chris Wheeler <cwheeler@zipcar.com>
2015-06-19 23:50:28 -0400

    changed port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var Hapi = require('hapi');
 var routes = require('./routes/routes');
 var server = new Hapi.Server();
-server.connection({ port: 3000 });
+server.connection({ port: 3001 });
 
 routes.forEach(function(v){
   server.route(v);
